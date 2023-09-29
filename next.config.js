838bfd7114cef04dbd881a5de4084260eceea1c0
sai kise <134133636+saikise@users.noreply.github.com>
2023-09-28 21:51:14 -0700

    Replace ESLint config for __tests__ directory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        // Reference: https://nextjs.org/docs/app/building-your-application/configuring/eslint#linting-custom-directories-and-files
+        // Include __tests__ in the default list of directories.
+        dirs: ["pages", "app", "components", "lib", "src", "__tests__"],
+    },
+};
 
 module.exports = nextConfig;
